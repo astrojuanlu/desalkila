a6f89fc421b05880c1e91cef225819b585383e26
Juan Luis Cano Rodríguez <hello@juanlu.space>
2024-08-09 14:33:47 +0200

    Add missing metadata to GeoPandas dataset

--- src/desalkila/datasets/geopandas.py
+++ src/desalkila/datasets/geopandas.py
@@ -7,9 +7,15 @@ from kedro.io import AbstractDataset
 
 
 class GeoPandasGenericDataset(AbstractDataset):
-    def __init__(self, filepath: str, load_args: dict[str, t.Any] | None = None):
+    def __init__(
+        self,
+        filepath: str,
+        load_args: dict[str, t.Any] | None = None,
+        metadata: dict[str, t.Any] | None = None,
+    ):
         self._filepath = filepath
         self._load_args = load_args or {}
+        self._metadata = metadata or {}
 
     def _load(self):
         return geopandas.read_file(self._filepath, **self._load_args)
