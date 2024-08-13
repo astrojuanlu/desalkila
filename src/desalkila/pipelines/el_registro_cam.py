f68fb5a2f18a144e2d98801ebb062e1d07edf5fc
Juan Luis Cano Rodríguez <hello@juanlu.space>
2024-08-13 13:59:15 +0200

    Do not exclude non-touristic homes

--- src/desalkila/pipelines/el_registro_cam.py
+++ src/desalkila/pipelines/el_registro_cam.py
@@ -2,11 +2,8 @@ import polars as pl
 
 
 def preprocess_registro_cam(df: pl.DataFrame) -> pl.DataFrame:
-    df = df.filter(
-        (pl.col("alojamiento_tipo") == "VIVIENDAS DE USO TU")
-        & (pl.col("localidad") == "Madrid")
-    ).select(
-        pl.col("signatura"),
-        pl.all().exclude("alojamiento_tipo", "categoria", "signatura"),
+    df = df.filter(pl.col("localidad") == "Madrid").select(
+        "signatura",
+        pl.all().exclude("signatura"),
     )
     return df
