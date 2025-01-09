8ca3bb9713ac87b6733e2bd952c66dd770309921
Juan Luis Cano Rodríguez <hello@juanlu.space>
2025-01-09 11:44:59 +0100

    WIP: Attempt WASM (it does not work)

--- app/index.js
+++ app/index.js
@@ -1,78 +1,73 @@
 import { MapboxOverlay } from "@deck.gl/mapbox";
-import { GeoJsonLayer } from "@deck.gl/layers";
+import { GeoArrowScatterplotLayer } from "@geoarrow/deck.gl-layers";
 import maplibregl from "maplibre-gl";
-import { asyncBufferFromUrl, parquetMetadataAsync } from "hyparquet";
-import { toGeoJson } from "geoparquet";
-import proj4 from "proj4";
+import { tableFromIPC } from "apache-arrow";
+import { readGeoParquet } from "@geoarrow/geoparquet-wasm";
 import Converter from "projjson-to-wkt";
+// import proj4 from "proj4";
 
-async function getSourceCrs(file) {
-  const metadata = await parquetMetadataAsync(file);
-  const geoMetadata = JSON.parse(
-    metadata.key_value_metadata?.find((kv) => kv.key === "geo").value,
-  );
+async function getSourceCrs(table) {
+  const geoMetadata = JSON.parse(table.schema.metadata.get("geo"));
   return Converter.toWkt1(geoMetadata.columns.geometry.crs);
 }
 
+async function loadGeoParquet(url) {
+  const resp = await fetch(url);
+  console.log(resp);
+  const arrayBuffer = await resp.arrayBuffer();
+  console.log(arrayBuffer);
+  const data = new Uint8Array(arrayBuffer);
+  console.log(data)
+  const wasmTable = readGeoParquet(data);
+  console.log(wasmTable);
+  return tableFromIPC(wasmTable.intoIPCStream());
+}
+
 async function initializeMap() {
-  try {
-    // Load and convert GeoParquet file
-    const url =
-      "https://raw.githubusercontent.com/astrojuanlu/desalkila/refs/heads/app/app/public/registry_cam_no_vuts_simple.geoparquet";
-    const file = await asyncBufferFromUrl({ url });
-    const sourceCrs = await getSourceCrs(file);
+  console.log("Starting");
 
-    var geojson = await toGeoJson({ file });
-    geojson = {
-      type: "FeatureCollection",
-      features: geojson.features.map((feature) => {
-        const reprojectedGeometry = {
-          type: feature.geometry.type,
-          coordinates: proj4(
-            sourceCrs,
-            "EPSG:4326",
-            feature.geometry.coordinates,
-          ),
-        };
-        return { ...feature, geometry: reprojectedGeometry };
-      }),
-    };
+  // Load and convert GeoParquet file
+  const url =
+    "https://raw.githubusercontent.com/astrojuanlu/desalkila/refs/heads/app/app/public/registry_cam_no_vuts_simple.geoparquet";
+  const table = await loadGeoParquet(url);
+  console.log(table);
+  const sourceCrs = await getSourceCrs(table);
+  console.log(sourceCrs);
 
-    const map = new maplibregl.Map({
-      container: "map",
-      style: 'https://tiles.openfreemap.org/styles/bright',
-      center: [-3.7038, 40.4168], // Madrid
-      zoom: 11,
+  const map = new maplibregl.Map({
+    container: "map",
+    style: "https://tiles.openfreemap.org/styles/bright",
+    center: [-3.7038, 40.4168], // Madrid
+    zoom: 11,
+  });
+  console.log(map);
+
+  map.on("load", () => {
+    // Initialize your deck instance
+    const deckOverlay = new MapboxOverlay({
+      layers: [
+        new GeoArrowScatterplotLayer({
+          id: "geojson-layer",
+          data: table,
+          filled: true,
+          getPosition: table.getChild("geometry"),
+          pointRadiusMinPixels: 4,
+          getPointRadius: 1,
+          getFillColor: [235, 147, 96, 200],
+          getLineColor: [171, 102, 62],
+          pickable: true,
+          onHover: ({ object }) => {
+            if (object) {
+              console.log(object.properties);
+            }
+          },
+        }),
+      ],
     });
 
-    map.on("load", () => {
-      // Initialize your deck instance
-      const deckOverlay = new MapboxOverlay({
-        layers: [
-          new GeoJsonLayer({
-            id: "geojson-layer",
-            data: geojson,
-            filled: true,
-            pointRadiusMinPixels: 4,
-            getPointRadius: 1,
-            getFillColor: [235, 147, 96, 200],
-            getLineColor: [171, 102, 62],
-            pickable: true,
-            onHover: ({ object }) => {
-              if (object) {
-                console.log(object.properties);
-              }
-            },
-          }),
-        ],
-      });
-
-      map.addControl(deckOverlay);
-      map.addControl(new maplibregl.NavigationControl());
-    });
-  } catch (error) {
-    console.error("Error initializing map:", error);
-  }
+    map.addControl(deckOverlay);
+    map.addControl(new maplibregl.NavigationControl());
+  });
 }
 
 initializeMap();
--- app/vite.config.js
+++ app/vite.config.js
@@ -1,7 +1,38 @@
 import { defineConfig } from "vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
+  plugins: [wasm(), topLevelAwait()],
   build: {
-    sourcemap: false,
-  },
+    sourcemap: true,
+    assetsInlineLimit: 0,  // Because fuck you, that's why
+  }
 });
+
+  //   target: "esnext",
+  //   modulePreload: {
+  //     polyfill: false,
+  //   },
+  //   rollupOptions: {
+  //     // This ensures proper WASM loading in different environments
+  //     output: {
+  //       manualChunks: {
+  //         geoparquet: ["@geoarrow/geoparquet-wasm"],
+  //       },
+  //     },
+  //   },
+  // },
+  // optimizeDeps: {
+  //   // This is needed to prevent Vite from trying to bundle WASM modules during dev
+  //   exclude: ["@geoarrow/geoparquet-wasm"],
+  // },
+  // resolve: {
+  //   alias: {
+  //     // This allows the module loader to work in both dev and prod
+  //     "@geoarrow/geoparquet-wasm":
+  //       process.env.NODE_ENV === "production"
+  //         ? "@geoarrow/geoparquet-wasm/esm/index.js"
+  //         : "@geoarrow/geoparquet-wasm/node/index.js",
+  //   },
+  // },
